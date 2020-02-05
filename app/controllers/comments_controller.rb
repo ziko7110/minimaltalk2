8717377f656ae1392b669ccf1275efdba27788a4
ziko7110 <ziko7110@gmail.com>
2020-02-05 21:59:14 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,14 @@ class CommentsController < ApplicationController
     redirect_to "/items/#{comment.item.id}" 
   end
 
+  def destroy
+# binding.pry
+    # Comment.find_by(params[:id])
+    comment = Comment.find(params[:id],params[:item_id])
+    comment.destroy
+    redirect_to root_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment, :comment_nickname, :likes, :bad).merge( item_id: params[:item_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 root "items#index"
 resources :items do
-  resources :comments , only: :create
+  resources :comments , only: [:create]
 end
+resources :comments, only: [:destroy]
 # get 'items/new', to: 'items#new'
 # post 'items/create', to: 'items#create'
 end

1aed2db94289d54170146069472271ef0a18dca3
ziko7110 <ziko7110@gmail.com>
2020-02-18 16:22:04 +0900

    search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,10 @@ class ItemsController < ApplicationController
     item.destroy
     redirect_to root_path
   end
+
+  def search
+    @items = Item.search(params[:keyword])
+  end
   
   private
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, :text, presence: true
   has_many :comments 
+
+  def self.search(search)
+    return Item.all unless search
+    Item.where('title LIKE(?)', "%#{search}%")
+  end
+
 end
